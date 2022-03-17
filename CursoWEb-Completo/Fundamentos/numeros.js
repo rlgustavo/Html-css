d00d407942b4b615dfe3f4014e67e186a38756a4
Gustavo R Lima <rlgustavo1927@gmail.com>
2022-03-17 09:38:37 -0300

    Number em Js
    Commit da aula de JS

---
+++ CursoWEb-Completo/Fundamentos/numeros.js
@@ -0,0 +1,10 @@
+const peso = 1.0;
+const peso2 = Number('2.0');
+
+console.log(peso, peso2);
+console.log(Number.isInteger(peso));
+console.log(Number.isInteger(peso2));
+
+
+console.log(7 / 0);
+console.log("10" / 2)
